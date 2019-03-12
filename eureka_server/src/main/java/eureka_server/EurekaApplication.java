6c03b8bf37b79dbe6b8cd4786957b7c0be3e3492
zongtong <zongtong@yuntongxun.com>
2019-03-12 16:21:53 +0800

    test_demo

---
+++ eureka_server/src/main/java/eureka_server/EurekaApplication.java
@@ -0,0 +1,12 @@
+package eureka_server;/*
+ * Copyright (C), 2015-2019, 壹永科技有限公司
+ * FileName: EurekaApplication
+ * Author:   zongtong
+ * Date:     2019/3/3 下午5:38
+ * History:
+ * <author>          <time>                <version>
+ * zongtong           2019/3/3 下午5:38           v1.0.0
+ */
+
+public class EurekaApplication {
+}
